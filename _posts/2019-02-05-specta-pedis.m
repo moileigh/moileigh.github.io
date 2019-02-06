97411b6597cde56a0ca45fde3048ed47c88eb0ac
moileigh <36524964+moileigh@users.noreply.github.com>
2019-02-05 21:11:29 -0500

    Create 2019-02-05-specta-pedis.m

---
+++ _posts/2019-02-05-specta-pedis.m
@@ -0,0 +1,54 @@
+---
+title: "Specta Pedis"
+layout: post
+categories:
+  - Daily Life
+---
+In today’s devotional reading I came across Isaiah chapter 52, verse 7. 
+It reads, “How beautiful upon the mountains are the feet of the messenger who announces peace, who brings good news, who announces salvation, who says to Zion, ‘Your God reigns.’”
+
+I came home to the states for tenth grade. 
+It was a furlough year, when my parents did a lot of traveling around the country visiting their supporting churches. 
+Our home base was in East Brookfield, MA, and my brother and I attended the same school district that we’d gone to as elementary children back before my parents became missionaries. 
+It was my brother’s senior year. 
+
+When it was time to choose a foreign language to study, I chose French for an easy A. 
+I pretty much spent the year correcting my poor teacher’s accent and teaching her new words. 
+My best friend was taking Latin. 
+We tried to teach each other some phrases but nothing really stuck. 
+Except for two words: specta pedis. 
+It means “Look at the feet.” 
+
+My boyfriend at the time had little feet stickers stuck onto his guitar. 
+My friend, in the process of studying her flashcards, pointed to the feet and said, “Specta pedis.” 
+It turned into one of those often repeated inside jokes, and those two Latin words became firmly ensconced in my vocabulary.
+
+Can you see where I’m going here? 
+Did you notice how these two stories fit together? 
+When I was reading the passage in Isaiah, I had this picture in my head of someone saying, “Specta pedis,” and looking at the feet of messenger climbing the mountains to bring peace and good news to the men, women, and children there. 
+How beautiful are the feet of that messenger.
+
+I’ve never seen any feet that I would classify as beautiful. 
+And really I don’t want anyone looking at my feet after I’ve hiked up a mountain.  
+But do I understand the idea. 
+When you’re miserable, alone, or depressed and someone brings you news that can rock your world and solve your problems, they are beautiful.
+
+Wes (who created and helps maintain my website) asked me to write up a paragraph explaining the name of my blog and my goals and purposes for the website. 
+
+I named the website scribblinghope for a couple of reasons. 
+The first is most obvious – anyone who’s seen my handwriting would definitely describe it as a bit of a scribble. 
+I sometimes call it chicken scratching. 
+But I also like the word scribble because scribbling implies that it’s not always carefully crafted and thought out. 
+It’s not professional. 
+It’s not perfect. 
+Sometimes my writing is messy and chaotic. 
+This isn’t a formal site meant to look perfectly polished. 
+It’s just a bit of a scribble as I put my thoughts out there for the world to read. 
+But the end goal is hope. 
+I’m hoping that my words will bring a bit of Hope into a tired world. 
+That somehow, by sharing my thoughts, I can light a candle in someone else’s darkness. 
+That maybe my story will remind someone else that their story can be amazingly beautiful. 
+And that together, our stories can change our worlds. 
+I want to use my scribbling to share Hope, announce Peace, bring Good News, and announce Salvation. 
+
+I want to have beautiful feet.
